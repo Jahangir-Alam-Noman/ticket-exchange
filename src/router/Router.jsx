8d35f4c2e276b8aa888acaac5a21f33847c2a5b3
jahangir <jahangir2km@gmail.com>
2022-06-19 14:19:06 +0600

    Header part added

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,14 +1,18 @@
+/** @format */
+
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Footer from "../components/Shared/Footer/Footer";
+import Header from "../components/Shared/Header/Header";
 import Home from "../pages/Home";
 
 const Router = () => {
   return (
     <div>
       <BrowserRouter>
+        <Header></Header>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path='/' element={<Home />}></Route>
         </Routes>
         <Footer></Footer>
       </BrowserRouter>
